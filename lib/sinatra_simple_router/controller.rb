3691eb7a18616a89c43935ddf849666a3bab4a60
elvio <elvio@elviovicosa.com>
2013-09-26 16:13:06 +0200

    Add render support for Controller

--- lib/sinatra_simple_router/controller.rb
+++ lib/sinatra_simple_router/controller.rb
@@ -4,6 +4,22 @@ module SinatraSimpleRouter
       @app = app
     end
 
+    def render(options = {})
+      if code=options[:status].to_i and code > 0
+        status code
+      end
+
+      if html=options[:html]
+        content_type "text/html"
+        body html.to_s
+      elsif json=options[:json]
+        content_type "application/json"
+        body json.to_json
+      else
+        raise ArgumentError.new("Unknown format in #{options.keys.sort}")
+      end
+    end
+
     def method_missing(method, *args, &block)
       if @app.respond_to?(method)
         (self.instance_variables - [:"@app"]).each do |v|
--- spec/sinatra_simple_router/controller_spec.rb
+++ spec/sinatra_simple_router/controller_spec.rb
@@ -3,10 +3,37 @@ require "sinatra_simple_router"
 
 module SinatraSimpleRouter
   describe Controller do
-    describe "method delegation" do
-      let(:app) { double("App", body: "") }
-      let(:controller) { ItemsController.new(app) }
+    let(:app) { double("App", body: "") }
+    let(:controller) { ItemsController.new(app) }
 
+    describe "#render" do
+      let(:content) { double("Content") }
+      let(:json) { double("JSON") }
+
+      it "renders json" do
+        expect(app).to receive(:content_type).with("application/json")
+        expect(app).to receive(:body).with(json)
+        expect(content).to receive(:to_json).and_return(json)
+
+        controller.render json: content
+      end
+
+      it "renders html" do
+        expect(app).to receive(:content_type).with("text/html")
+        expect(app).to receive(:body).with(content)
+        expect(content).to receive(:to_s).and_return(content)
+
+        controller.render html: content
+      end
+
+      it "sets the status" do
+        app.stub(:content_type)
+        expect(app).to receive(:status).with(201)
+        controller.render html: "", status: 201
+      end
+    end
+
+    describe "method delegation" do
       context "when the method is present in the Sinatra class" do
         it "delegates to the class" do
           expect(app).to receive(:body)
